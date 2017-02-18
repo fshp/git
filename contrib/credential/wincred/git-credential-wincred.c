1403b5356240993578414f66d46de4e69468109a
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2017-02-18 20:50:47 +0300

    10

--- contrib/credential/wincred/git-credential-wincred.c
+++ contrib/credential/wincred/git-credential-wincred.c
@@ -16,7 +16,7 @@ static void die(const char *err, ...)
 	char msg[4096];
 	va_list params;
 	va_start(params, err);
-	vsnprintf(msg, sizeof(msg), err, params);
+	xvsnprintf(msg, sizeof(msg), err, params);
 	fprintf(stderr, "%s\n", msg);
 	va_end(params);
 	exit(1);
