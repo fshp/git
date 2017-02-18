ed03895f645997d61e96c7bce5394b78e9ec0f76
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2017-02-18 20:49:58 +0300

    3

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -320,7 +320,7 @@ static void report_message(const char *prefix, const char *err, va_list params)
 	char msg[4096];
 
 	sz = xsnprintf(msg, sizeof(msg), "%s", prefix);
-	sz += vsnprintf(msg + sz, sizeof(msg) - sz, err, params);
+	sz += xvsnprintf(msg + sz, sizeof(msg) - sz, err, params);
 	if (sz > (sizeof(msg) - 1))
 		sz = sizeof(msg) - 1;
 	msg[sz++] = '\n';
